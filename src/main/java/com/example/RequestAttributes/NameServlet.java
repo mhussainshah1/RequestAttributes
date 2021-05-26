15e61f2c9cc27257ef09042b2aed2aebd55c3949
Muhammad Shah <muhammadshah.14@sunymaritime.edu>
2021-05-26 08:21:02 -0400

    Initial commit

---
+++ src/main/java/com/example/RequestAttributes/NameServlet.java
@@ -0,0 +1,45 @@
+package com.example.RequestAttributes;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+//http://localhost:8080/name?firstName=Amir&lastNamer=Shah&age=20
+@WebServlet(name = "NameServlet", urlPatterns = "/name")
+public class NameServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String age = request.getParameter("age");
+
+        StringBuilder error = new StringBuilder();
+        if (firstName == null) {
+            error.append("A first name was not provided.");
+        } else if (lastName == null) {
+            error.append("A last name was not provided.");
+        } else if (age == null) {
+            error.append("An age was not provided.");
+        }
+
+        if (error.length() > 0) {
+            request.setAttribute("error", error.toString());
+        } else {
+            request.setAttribute("firstName", firstName);
+            request.setAttribute("lastName", lastName);
+            request.setAttribute("age", age);
+        }
+        getServletContext().getRequestDispatcher("/other").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    }
+}
---
+++ src/main/webapp/name.jsp
@@ -0,0 +1,25 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Owner
+  Date: 5/25/2021
+  Time: 11:31 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Name Page</title>
+</head>
+<body>
+<H1>Name Master 2000 </H1>
+
+<hr>
+
+<p>${error}</p> <%--expression language--%>
+
+<p>Hey ${firstName} ${lastName} , nice to meet you.</p>
+
+<p>You are ${age} years old.</p>
+
+</body>
+</html>
